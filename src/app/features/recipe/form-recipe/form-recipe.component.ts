35d8e75e44b736684d57c048a7171f3e28a1a7e6
Natalia Roque <nsbnroque@gmail.com>
2023-06-18 12:13:48 -0300

    Modificacoes no Form de recipe

--- src/app/features/recipe/form-recipe/form-recipe.component.ts
+++ src/app/features/recipe/form-recipe/form-recipe.component.ts
@@ -9,46 +9,62 @@ import { Ingredient } from 'src/app/core/models/ingredient';
 })
 export class FormRecipeComponent implements OnInit{
   ingredients: Ingredient[] = [];
+  steps: any[] =[];
   recipeForm!: FormGroup;
+  directionsForm!: FormGroup;
+  ingredientForm!: FormGroup;
 
-  firstFormGroup = this.builder.group({
-    firstCtrl: ['', Validators.required],
-  });
-  secondFormGroup = this.builder.group({
-    secondCtrl: ['', Validators.required],
-  });
   isLinear = false;
 
   constructor(private builder: FormBuilder) {}
 
   ngOnInit(): void {
     this.addIngredient();
+    this.addStep();
     this.initRecipeForm();
+    
   }
 
   initRecipeForm(): void {
     this.recipeForm = this.builder.group({
       Id: [null],
       name: ['', Validators.required],
-      image: ['', Validators.required],
-      preparationTime: [null, Validators.required],
-      cookTime: [null, Validators.required],
-      totalTime: [null, Validators.required],
-      servings: [null, Validators.required],
+      image: [''],
+      preparationTime: ['', [Validators.pattern(/^\d+$/)]],
+      cookTime: ['', [ Validators.pattern(/^\d+$/)]],
+      totalTime: ['', [ Validators.pattern(/^\d+$/)]],
+      servings: ['', [Validators.pattern(/^\d+$/)]],
       directions: this.builder.array([]),
       ingredients: this.builder.array([]),
       author: this.builder.group({
         // Define os campos do autor aqui
       })
     });
-  }
 
-  createIngredientFormGroup(ingredient: Ingredient): FormGroup {
-    return new FormGroup({
-      name: new FormControl(ingredient.name),
-      quantity: new FormControl(ingredient.quantity)
+    this.directionsForm = this.builder.group({
+      step: ['']
+    });
+
+    this.ingredientForm = this.builder.group({
+      Id: [null],
+      name: [''],
+      quantity:['']
     });
   }
+
+  // createIngredientFormGroup(ingredient: Ingredient): FormGroup {
+  //   return new FormGroup({
+  //     name: new FormControl(ingredient.name),
+  //     quantity: new FormControl(ingredient.quantity)
+  //   });
+  // }
+
+  // createDirectionsFormGroup(step : String): FormGroup {
+  //   return new FormGroup({
+  //     step: new FormControl(step),
+
+  //   });
+  // }
 
   addIngredient() {
     const newIngredient: Ingredient = {
@@ -59,11 +75,29 @@ export class FormRecipeComponent implements OnInit{
     this.ingredients.push(newIngredient);
   }
 
+  addStep() {
+    const newStep = {
+      step: ''
+    };
+    this.steps.push(newStep);
+  }
+
   deleteIngredient(ingredient: Ingredient) {
     if (this.ingredients.length > 1){
       const index = this.ingredients.indexOf(ingredient);
     if (index !== -1) {
       this.ingredients.splice(index, 1);
+    }
+    return;
+    }
+    
+  }
+
+  removeStep(step : any) {
+    if (this.steps.length > 1){
+      const index = this.steps.indexOf(step);
+    if (index !== -1) {
+      this.steps.splice(index, 1);
     }
     return;
     }
